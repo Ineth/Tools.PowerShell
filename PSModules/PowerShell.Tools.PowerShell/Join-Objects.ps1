a0411881c0e7a6540c74e34018e48523d06dee5f
Thomas De Pauw <thomas.depauw@euri.com>
2017-03-29 17:04:21 +0200

    Optimized for CSV export and created an object join

---
+++ PSModules/PowerShell.Tools.PowerShell/Join-Objects.ps1
@@ -0,0 +1,24 @@
+﻿function Join-Objects([PSObject[]] $Objects, [switch] $returnHashTable) {
+    $objProperties = [ordered]@{}
+    foreach ($obj in $objects) {
+        foreach($prop in $obj.PSObject.Properties | Where-Object { $_.MemberType -eq 'NoteProperty'}) {
+            $keyName = $prop.Name
+            if($objProperties.Contains($prop.Name)){
+                $suffix = 1
+                $newName = $prop.Name+$suffix
+                while($objProperties.Contains($newName)){
+                 $suffix++
+                 $newName = $prop.Name+$suffix
+                }
+                $keyName = $newName
+            }
+           
+            $objProperties.Add($keyName, $prop.Value)
+        } 
+    }
+    if($returnHashTable) {
+        return $objProperties
+    }        
+    New-Object -TypeName PSCustomObject -Property $objProperties
+
+} 
--- PSScripts/ApiPollingFakeStates/HttpRequest.ps1
+++ PSScripts/ApiPollingFakeStates/HttpRequest.ps1
@@ -8,51 +8,86 @@ function Write-DataRow($object) {
     }
 }
 
-$loops = 200
-$results = @()
-$mgtPtOperationResults = @()
-$mgtPtStatusResults = @()
-#for($i = 0; $i -lt $loops; $i++) {
-$i = 0
-while($true) {
-    $i++
-    $CurrentDateTime = Get-Date -Format g
-
+function Get-MgmtPointInfo($lineNo) {
     $result = Invoke-WebRequest "http://localhost:9092/api/controllers/LL1NTEST01/mgmtpoints/187" | 
     ConvertFrom-Json  |
     select -ExpandProperty data |
     select *
     
-    $result | Add-Member -Type NoteProperty -Name LogDT -Value $CurrentDateTime
-    $result | Add-Member -Type NoteProperty -Name LineNo -Value $i
-    $results += $result | select LineNo, LogDT, id, onOffMode, operationMode, roomTemp, setpoint, inMaintenanceMode, inEquipmentError, inCommunicationError
+    $result | Add-Member -Type NoteProperty -Name LineNo -Value $lineNo
+    $result | Add-Member -Type NoteProperty -Name LogDT -Value (Get-Date -Format $dateTimeFormat)
+    #$results += $result | select LineNo, LogDT, id, onOffMode, operationMode
 
-    
+    return $result | select LineNo, LogDT, id, onOffMode , operationMode
+}
+
+function Get-MgmtPointOperationData($lineNo) {
     $mgtPtOperationResult = Invoke-MySql -ConnectionString $ConnectionString -Query "SELECT * FROM itabcadm.mgmtpt_operating where MGMTPT_PK = 187"
     
-    $mgtPtOperationResult | Add-Member -Type NoteProperty -Name LogDT -Value $CurrentDateTime
-    $mgtPtOperationResult | Add-Member -Type NoteProperty -Name LineNo -Value $i
+    $mgtPtOperationResult | Add-Member -Type NoteProperty -Name LineNo -Value $lineNo
+    $mgtPtOperationResult | Add-Member -Type NoteProperty -Name LogDT -Value (Get-Date -Format $dateTimeFormat)
+    
     Write-DataRow $mgtPtOperationResult
-    $mgtPtOperationResults += $mgtPtOperationResult
+    #$mgtPtOperationResults += $mgtPtOperationResult
+    
+    return $mgtPtOperationResult | select LineNo, LogDT, SENDSTATUS, CHANGEON_OFF, ONOFF, CHANGEOPMODE, OPERATIONMODE
+}
 
+function Get-MgmtPointStatusData($lineNo) {    
     $mgtPtStatusResult = Invoke-MySql -ConnectionString $ConnectionString -Query "SELECT * FROM itabcadm.mgmtpt_status where MGMTPT_PK = 187"
     
-    $mgtPtStatusResult | Add-Member -Type NoteProperty -Name LogDT -Value $CurrentDateTime
-    $mgtPtStatusResult | Add-Member -Type NoteProperty -Name LineNo -Value $i
+    $mgtPtStatusResult | Add-Member -Type NoteProperty -Name LineNo -Value $lineNo
+    $mgtPtStatusResult | Add-Member -Type NoteProperty -Name LogDT -Value (Get-Date -Format $dateTimeFormat)
     $mgtPtStatusResults += $mgtPtStatusResult
 
-    Start-Sleep -Milliseconds 300
+    return $mgtPtStatusResult | select LineNo3, LogDT, RECEIVEDT, REPORTDT, ON_OFF, OPERATION_MODE 
+}
+
+$loops = 200
+$results = @()
+$mgtPtOperationResults = @()
+$mgtPtStatusResults = @()
+$dataRows = @()
+$dataRowObjects = @()
+$dateTimeFormat = 'HH:mm:ss.fff'
+
+#for($i = 0; $i -lt $loops; $i++) {
+$i = 0
+while($true) {
+    $i++
+    $dataRowObjects = @()
+
+    $dataRowObjects += Get-MgmtPointInfo -lineNo $i    
+    $dataRowObjects += Get-MgmtPointOperationData -lineNo $i
+    $dataRowObjects += Get-MgmtPointStatusData -lineNo $i
+
+    Start-Sleep -Milliseconds 200
     if(($i % 15) -eq 0) {
         #Write-Progress -Activity "Gathering api data for $loops loops" -PercentComplete (($i / $loops) * 100)
     }
+
+    $dataRows += Join-Objects -Objects $dataRowObjects
 }
 
-$results | Out-GridView
-$mgtPtOperationResults | Out-GridView
-$mgtPtStatusResults | Out-GridView
+#$results | Out-GridView
+#$mgtPtOperationResults | Out-GridView
+#$mgtPtStatusResults | Out-GridView
+$dataRows | Out-GridView
+
+$dataRows | Export-Csv  -Path $("mgmtPoint-monitoring $(Get-Date -Format 'HH-mm-ss') ($($i) records).csv") -NoTypeInformation -Force
 
 
-
-
-
-
+#$newObject = @{}
+#$test = $mgtPtOperationResult | select ON_OFF, OPERATION_Mode 
+#$test.PSObject.Properties |
+#foreach {
+#    Write-Host $_.Name $_.Value
+#    $newObject.Add($_.Name,$_.value)
+#}
+#
+#New-Object -TypeName PSCustomObject -Property $newObject | Export-Csv -Path 'myData.csv' -NoTypeInformation -Force
+#
+#$myObjects = @()
+#$myObjects += $mgtPtOperationResult | select ON_OFF, OPERATION_Mode
+#$myObjects += $result | select onoffMode, operationMode
+#Merge-Objects -objects $myObjects
