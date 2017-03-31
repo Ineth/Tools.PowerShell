190a899c7ffff3c3881f1dae5b1cabac07245279
Thomas De Pauw <thomas.depauw@euri.com>
2017-03-31 13:53:39 +0200

    Cleanup test script

--- PSScripts/ApiPollingFakeStates/HttpRequest.ps1
+++ PSScripts/ApiPollingFakeStates/HttpRequest.ps1
@@ -1,5 +1,16 @@
 ﻿. (join-path $PSScriptroot './Load-Credentials.ignore.ps1')
 
+
+$results = @()
+$mgtPtOperationResults = @()
+$mgtPtStatusResults = @()
+$dataRows = @()
+$dataRowObjects = @()
+$dateTimeFormat = 'HH:mm:ss.fff'
+$mySqlDateFormat = "%e/%c/%y %T"
+
+# Load subfunctions
+
 function Write-DataRow($object) {
     if(($object.LineNo % 6 -ne 0)) {
         $object | Format-Table -HideTableHeaders
@@ -16,78 +27,67 @@ function Get-MgmtPointInfo($lineNo) {
     
     $result | Add-Member -Type NoteProperty -Name LineNo -Value $lineNo
     $result | Add-Member -Type NoteProperty -Name LogDT -Value (Get-Date -Format $dateTimeFormat)
-    #$results += $result | select LineNo, LogDT, id, onOffMode, operationMode
+    $results += $result | select LineNo, LogDT, id, onOffMode, operationMode
 
-    return $result | select LineNo, LogDT, id, onOffMode , operationMode
+    return $result | select LineNo, LogDT, id, onOffMode , isOnOffFakeState, operationMode, isOperationModeFakeState
 }
 
 function Get-MgmtPointOperationData($lineNo) {
-    $mgtPtOperationResult = Invoke-MySql -ConnectionString $ConnectionString -Query "SELECT * FROM itabcadm.mgmtpt_operating where MGMTPT_PK = 187"
+    $mgtPtOperationResult = Invoke-MySql -ConnectionString $ConnectionString -Query "SELECT *, DATE_FORMAT(UPDATE_DT,'$mySqlDateFormat') AS UPDATE_DT_F FROM itabcadm.mgmtpt_operating where MGMTPT_PK = 187"
     
     $mgtPtOperationResult | Add-Member -Type NoteProperty -Name LineNo -Value $lineNo
     $mgtPtOperationResult | Add-Member -Type NoteProperty -Name LogDT -Value (Get-Date -Format $dateTimeFormat)
     
     Write-DataRow $mgtPtOperationResult
-    #$mgtPtOperationResults += $mgtPtOperationResult
+    $mgtPtOperationResults += $mgtPtOperationResult
     
-    return $mgtPtOperationResult | select LineNo, LogDT, SENDSTATUS, CHANGEON_OFF, ONOFF, CHANGEOPMODE, OPERATIONMODE
+    return $mgtPtOperationResult | select LineNo, LogDT, UPDATE_DT, SEND_STATUS, CHANGE_ON_OFF, ON_OFF, CHANGE_OP_MODE, OPERATION_MODE
 }
 
 function Get-MgmtPointStatusData($lineNo) {    
-    $mgtPtStatusResult = Invoke-MySql -ConnectionString $ConnectionString -Query "SELECT * FROM itabcadm.mgmtpt_status where MGMTPT_PK = 187"
+    $mgtPtStatusResult = Invoke-MySql -ConnectionString $ConnectionString -Query "SELECT *, DATE_FORMAT(RECEIVE_DT,'$mySqlDateFormat') AS RECEIVE_DT_F, DATE_FORMAT(REPORT_DT,'$mySqlDateFormat') As REPORT_DT_F FROM itabcadm.mgmtpt_status where MGMTPT_PK = 187"
     
     $mgtPtStatusResult | Add-Member -Type NoteProperty -Name LineNo -Value $lineNo
     $mgtPtStatusResult | Add-Member -Type NoteProperty -Name LogDT -Value (Get-Date -Format $dateTimeFormat)
     $mgtPtStatusResults += $mgtPtStatusResult
 
-    return $mgtPtStatusResult | select LineNo3, LogDT, RECEIVEDT, REPORTDT, ON_OFF, OPERATION_MODE 
+    return $mgtPtStatusResult | select LineNo, LogDT, RECEIVE_DT_F, REPORT_DT_F, ON_OFF, OPERATION_MODE 
 }
 
-$loops = 200
-$results = @()
-$mgtPtOperationResults = @()
-$mgtPtStatusResults = @()
-$dataRows = @()
-$dataRowObjects = @()
-$dateTimeFormat = 'HH:mm:ss.fff'
+function Get-MgmtPointErrorData($lineNo) {
+    $mgtPointErrorResult = Invoke-MySql -ConnectionString $ConnectionString -Query "SELECT *, DATE_FORMAT(RECEIVE_DT,'$mySqlDateFormat') As RECEIVE_DT_F, DATE_FORMAT(REPORT_DT,'$mySqlDateFormat') As REPORT_DT_F FROM itabcadm.mgmtpt_error where MGMTPT_PK = 187"
+    
+    $mgtPointErrorResult | Add-Member -Type NoteProperty -Name LineNo -Value $lineNo
+    $mgtPointErrorResult | Add-Member -Type NoteProperty -Name LogDT -Value (Get-Date -Format $dateTimeFormat)
 
-#for($i = 0; $i -lt $loops; $i++) {
-$i = 0
-while($true) {
-    $i++
-    $dataRowObjects = @()
+    return $mgtPointErrorResult | select LineNo, LogDT, RECEIVE_DT_F, REPORT_DT_F, ERROR_OCCURENCE_DT, ERROR_TYPE
+}
 
-    $dataRowObjects += Get-MgmtPointInfo -lineNo $i    
-    $dataRowObjects += Get-MgmtPointOperationData -lineNo $i
-    $dataRowObjects += Get-MgmtPointStatusData -lineNo $i
+# Start logging
 
-    Start-Sleep -Milliseconds 200
-    if(($i % 15) -eq 0) {
-        #Write-Progress -Activity "Gathering api data for $loops loops" -PercentComplete (($i / $loops) * 100)
+Write-host $("Started at: $(Get-Date -Format $dateTimeFormat)") -ForegroundColor Green
+try {
+    $lineNo = 0
+    while($true) {
+        $lineNo++
+        $dataRowObjects = @()
+
+        $dataRowObjects += Get-MgmtPointInfo -lineNo $lineNo    
+        $dataRowObjects += Get-MgmtPointOperationData -lineNo $lineNo
+        $dataRowObjects += Get-MgmtPointStatusData -lineNo $lineNo
+        $dataRowObjects += Get-MgmtPointErrorData -lineNo $lineNo
+        $dataRows += Join-Objects -Objects $dataRowObjects
+
+        Start-Sleep -Milliseconds 100
     }
-
-    $dataRows += Join-Objects -Objects $dataRowObjects
+} finally {
+    write-Host 'Exporting data...'
+    $dataRows | Export-Csv  -Path $("mgmtPoint-monitoring $(Get-Date -Format 'HH-mm-ss') ($($i) records).csv") -NoTypeInformation -Force
 }
 
-#$results | Out-GridView
-#$mgtPtOperationResults | Out-GridView
-#$mgtPtStatusResults | Out-GridView
+# show seperate data output as gridview
+
+$results | Out-GridView
+$mgtPtOperationResults | Out-GridView
+$mgtPtStatusResults | Out-GridView
 $dataRows | Out-GridView
-
-$dataRows | Export-Csv  -Path $("mgmtPoint-monitoring $(Get-Date -Format 'HH-mm-ss') ($($i) records).csv") -NoTypeInformation -Force
-
-
-#$newObject = @{}
-#$test = $mgtPtOperationResult | select ON_OFF, OPERATION_Mode 
-#$test.PSObject.Properties |
-#foreach {
-#    Write-Host $_.Name $_.Value
-#    $newObject.Add($_.Name,$_.value)
-#}
-#
-#New-Object -TypeName PSCustomObject -Property $newObject | Export-Csv -Path 'myData.csv' -NoTypeInformation -Force
-#
-#$myObjects = @()
-#$myObjects += $mgtPtOperationResult | select ON_OFF, OPERATION_Mode
-#$myObjects += $result | select onoffMode, operationMode
-#Merge-Objects -objects $myObjects
