e7348a45050eb1dfe5bb03839574a0999e3d731c
Thomas De Pauw <thomas.depauw@euri.com>
2017-03-31 13:52:54 +0200

    Added Merge-Object function

---
+++ PSModules/PowerShell.Tools.PowerShell/Merge-Objects.ps1
@@ -0,0 +1,18 @@
+﻿function Merge-Objects([PSObject[]] $Objects, [switch] $returnHashTable) {
+    $objProperties = [ordered]@{}
+    foreach ($obj in $objects) {
+        foreach($prop in $obj.PSObject.Properties | Where-Object { $_.MemberType -eq 'NoteProperty'}) {
+            $keyName = $prop.Name
+            if(!$objProperties.Contains($prop.Name)){               
+                $objProperties[$prop.Name] = $prop.Value
+            } else {
+                $objProperties.Add($keyName, $prop.Value)
+            }           
+        } 
+    }
+    if($returnHashTable) {
+        return $objProperties
+    }        
+    New-Object -TypeName PSCustomObject -Property $objProperties
+
+} 
