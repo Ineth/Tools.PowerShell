b209a759b9a7d91b8917cd2e2aad253e44419fc1
Thomas De Pauw <Thomas.DePauw@xerius.be>
2018-05-02 09:45:41 +0200

    Added Test-Sql Connection string

---
+++ PSModules/PowerShell.Tools.SQL/Test-SqlConnectionString.ps1
@@ -0,0 +1,10 @@
+﻿function Test-SqlConnectionString($connectionString) {
+    $conn = New-Object System.Data.SqlClient.SqlConnection
+    $conn.ConnectionString = $connectionString
+
+    # If no error occurs here, then connection was successful.
+    Write-Host "Trying to connect..." -ForegroundColor Gray
+    $conn.Open();
+    $conn.Close();
+    Write-Host "Connection success" -ForegroundColor Green
+}
