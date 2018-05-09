01e2d566f8661689113800cb5569a65581361474
Thomas De Pauw <Thomas.DePauw@xerius.be>
2018-05-09 09:01:36 +0200

    example morning start script

---
+++ PSScripts/Start-MorningX2B.ps1
@@ -0,0 +1,8 @@
+﻿$projectPath = 'C:\\git\\X2B\\Xerius.Partner.Portaal\\Xerius.Partner.Portaal'
+$vsPath = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Enterprise\\Common7\\IDE\\devenv.exe'
+
+
+code $projectPath
+start powershell -WorkingDirectory $projectPath
+. $vsPath "C:\git\X2B\Xerius.Partner.Portaal\Xerius.Partner.Portaal.sln" 
+
